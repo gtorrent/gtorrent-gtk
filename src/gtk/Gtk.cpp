5822858fe548910c5ca2a617dd434e3e03c14cb8
truNEET <gtorrent.gentoo@gmail.com>
2014-07-22 14:57:12 -0500

    Run GtkMainWindow on the heap.
     - Automatically cleaned up after Gtk shutdown
     - Fixes Glibmm critical assertion error

--- src/gtk/Gtk.cpp
+++ src/gtk/Gtk.cpp
@@ -1,21 +1,10 @@
+#include "GtkMainWindow.hpp"
 #include "Gtk.hpp"
 
 GuiGtk::GuiGtk(int argc, char **argv)
 {
 	Gtk::Main kit(argc, argv);
-	mainWindow = new GtkMainWindow();
 
-	Gtk::Main::run();
-}
-
-GuiGtk::~GuiGtk()
-{
-	// You know, we could use auto and
-	// make_shared(...) instead so we still
-	// receive benefits of shared_ptr
-	// without the xbauxhueg mess
-
-	// This is called after Core::shutdown
-
-	delete mainWindow;
+	GtkMainWindow mainWindow;
+	kit.run(mainWindow);
 }
