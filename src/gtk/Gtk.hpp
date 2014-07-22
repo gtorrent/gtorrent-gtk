0ed65f8ee6f4ec457b7502cf277d477f261c2291
truNEET <gtorrent.gentoo@gmail.com>
2014-07-22 14:55:48 -0500

    Remove Gtk destructor and raw GtkMainWindow pointer

--- src/gtk/Gtk.hpp
+++ src/gtk/Gtk.hpp
@@ -1,15 +1,12 @@
 #pragma once
 
-#include <core/Base.hpp>
 #include "GtkMainWindow.hpp"
 #include <gtkmm/settings.h>
+#include <core/Base.hpp>
 #include <gtkmm/main.h>
 
 class GuiGtk : public GuiBase
 {
-private:
-	GtkMainWindow *mainWindow;
 public:
 	GuiGtk(int argc, char **argv);
-	~GuiGtk();
 };
