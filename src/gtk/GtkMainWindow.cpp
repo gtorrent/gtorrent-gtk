53600db5e6463d153bc9afdab61789acada31e52
Colin Atkinson <colin.william.atkinson@gmail.com>
2014-07-22 23:56:42 -0400

    Improve toolbar aesthetics
    Switch it to an actual GTK toolbar instead of a bunch of buttons
    Add a title to the header

--- src/gtk/GtkMainWindow.cpp
+++ src/gtk/GtkMainWindow.cpp
@@ -7,6 +7,7 @@
 #include <glibmm.h>
 #include <giomm.h>
 #include <boost/algorithm/string.hpp>
+#include <gtkmm/uimanager.h>
 
 GtkMainWindow::GtkMainWindow() :
 	m_core(Application::getSingleton()->getCore())
@@ -22,53 +23,50 @@ GtkMainWindow::GtkMainWindow() :
 
 	header = Gtk::manage(new Gtk::HeaderBar());
 	header->set_show_close_button(true);
+	header->set_title("gTorrent");
 	//TODO: add max/minimise buttons, next to the close button
 	//header->set_decoration_layout(
 	//		btn_connect,btn_add_torrent,btn_add_link,btn_up,btn_down,btn_pause,
 	//		btn_remove:maximise,minimize,close);
 	//TODO:Here's a nifty connect button to hang connection settings, rate limiter from.
-	Gtk::Button *btn_add_link    = Gtk::manage(new Gtk::Button());
-	Gtk::Button *btn_add_torrent = Gtk::manage(new Gtk::Button());
-	Gtk::Button *btn_connect     = Gtk::manage(new Gtk::Button());
-	Gtk::Button *btn_down        = Gtk::manage(new Gtk::Button());
-	Gtk::Button *btn_pause       = Gtk::manage(new Gtk::Button());
-	Gtk::Button *btn_properties  = Gtk::manage(new Gtk::Button());
-	Gtk::Button *btn_remove      = Gtk::manage(new Gtk::Button());
-	Gtk::Button *btn_resume      = Gtk::manage(new Gtk::Button());
-	Gtk::Button *btn_up          = Gtk::manage(new Gtk::Button());
-	Gtk::VSeparator *separator0  = Gtk::manage(new Gtk::VSeparator());
-	Gtk::VSeparator *separator1  = Gtk::manage(new Gtk::VSeparator());
-	Gtk::VSeparator *separator2  = Gtk::manage(new Gtk::VSeparator());
+	Glib::RefPtr<Gtk::ActionGroup> action_group = Gtk::ActionGroup::create();
+	action_group->add(Gtk::Action::create("Add Link", Gtk::Stock::PASTE), sigc::mem_fun(*this, &GtkMainWindow::onAddMagnetBtnClicked));
+	action_group->add(Gtk::Action::create("Add Torrent", Gtk::Stock::ADD), sigc::mem_fun(*this, &GtkMainWindow::onAddBtnClicked));
+	action_group->add(Gtk::Action::create("Connect", Gtk::Stock::DIRECTORY));
+	action_group->add(Gtk::Action::create("Pause", Gtk::Stock::MEDIA_PAUSE), sigc::mem_fun(*this, &GtkMainWindow::onPauseBtnClicked));
+	action_group->add(Gtk::Action::create("Resume", Gtk::Stock::MEDIA_PLAY), sigc::mem_fun(*this, &GtkMainWindow::onResumeBtnClicked));
+	action_group->add(Gtk::Action::create("Remove", Gtk::Stock::CANCEL), sigc::mem_fun(*this, &GtkMainWindow::onRemoveBtnClicked));
+	action_group->add(Gtk::Action::create("Down", Gtk::Stock::GO_DOWN));
+	action_group->add(Gtk::Action::create("Up", Gtk::Stock::GO_UP));
+	action_group->add(Gtk::Action::create("Properties", Gtk::Stock::PROPERTIES));
 
-	btn_add_link->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onAddMagnetBtnClicked));
-	btn_add_torrent->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onAddBtnClicked));
-	btn_pause->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onPauseBtnClicked));
-	btn_resume->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onResumeBtnClicked));
+	Glib::RefPtr<Gtk::UIManager> ui_manager = Gtk::UIManager::create();
+	ui_manager->insert_action_group(action_group);
+	add_accel_group(ui_manager->get_accel_group());
 
-	btn_add_link->set_image_from_icon_name    ("edit-paste");
-	btn_add_torrent->set_image_from_icon_name ("gtk-add");
-	btn_connect->set_image_from_icon_name     ("gtk-directory");
-	btn_down->set_image_from_icon_name        ("gtk-go-down");
-	btn_pause->set_image_from_icon_name       ("media-playback-pause");
-	btn_properties->set_image_from_icon_name  ("gtk-properties");
-	btn_remove->set_image_from_icon_name      ("gtk-cancel");
-	btn_resume->set_image_from_icon_name      ("media-playback-start");
-	btn_up->set_image_from_icon_name          ("gtk-go-up");
+	Glib::ustring ui_info =
+	    "<ui>"
+	    "	<toolbar  name='ToolBar'>"
+	    "		<toolitem action='Add Link' />"
+	    "		<toolitem action='Add Torrent' />"
+	    "		<toolitem action='Connect' />"
+	    "		<separator />"
+	    "		<toolitem action='Pause' />"
+	    "		<toolitem action='Resume' />"
+	    "		<toolitem action='Remove' />"
+	    "		<separator />"
+	    "		<toolitem action='Down' />"
+	    "		<toolitem action='Up' />"
+	    "		<toolitem action='Properties' />"
+	    "		<separator />"
+	    "	</toolbar>"
+	    "</ui>";
 
-	//TODO:align properties button to right of top bar
-	//btn_properties->set_alignment(1.0f,0.0f);
-	header->add(*btn_add_torrent);
-	header->add(*btn_add_link);
-	header->add(*btn_connect);
-	header->add(*separator0);
-	header->add(*btn_pause);
-	header->add(*btn_resume);
-	header->add(*btn_remove);
-	header->add(*separator1);
-	header->add(*btn_down);
-	header->add(*btn_up);
-	header->add(*btn_properties);
-	header->add(*separator2);
+	ui_manager->add_ui_from_string(ui_info);
+
+	Gtk::Widget* pToolBar = ui_manager->get_widget("/ToolBar");
+	pToolBar->override_background_color(Gdk::RGBA("0, 0, 0, 0"));
+	header->add(*pToolBar);
 
 	// Let's add some DnD goodness
 	vector<Gtk::TargetEntry> listTargets;
