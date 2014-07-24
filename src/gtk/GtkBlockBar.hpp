c6ff82d95802e3bb92655e0aaca70a6afd6e6cd3
Jibril Saffi <igi.222@gmail.com>
2014-07-24 14:27:11 +0200

    add BlockBar widget

---
+++ src/gtk/GtkBlockBar.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <gtkmm/widget.h>
+#include <gtkmm/cssprovider.h>
+
+class GtkBlockBar : public Gtk::Widget
+{
+public:
+  GtkBlockBar();
+  virtual ~GtkBlockBar();
+  void setBlocks(vector<bool> b);
+
+
+protected:
+
+  //Overrides:
+  virtual Gtk::SizeRequestMode get_request_mode_vfunc() const;
+  virtual void get_preferred_width_vfunc(int& minimum_width, int& natural_width) const;
+  virtual void get_preferred_height_for_width_vfunc(int width, int& minimum_height, int& natural_height) const;
+  virtual void get_preferred_height_vfunc(int& minimum_height, int& natural_height) const;
+  virtual void get_preferred_width_for_height_vfunc(int height, int& minimum_width, int& natural_width) const;
+  virtual void on_size_allocate(Gtk::Allocation& allocation);
+  virtual void on_map();
+  virtual void on_unmap();
+  virtual void on_realize();
+  virtual void on_unrealize();
+  virtual bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr);
+
+  Glib::RefPtr<Gdk::Window> m_refGdkWindow;
+  Glib::RefPtr<Gtk::CssProvider> m_refStyleProvider;
+
+  int m_scale;
+private:
+    vector<bool> m_blocks;
+};
