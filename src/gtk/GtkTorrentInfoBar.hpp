f43095689d79d6b07f3a47958c681a649d153984
Quaker762 <Jooster669@gmail.com>
2014-07-26 23:33:04 +1000

    Added m_progress to InfoBar

--- src/gtk/GtkTorrentInfoBar.hpp
+++ src/gtk/GtkTorrentInfoBar.hpp
@@ -5,6 +5,7 @@
 #include <gtkmm/box.h>
 #include <gtkmm/notebook.h>
 #include <gtkmm/label.h>
+#include <gtkmm/listbox.h>
 
 #include <Application.hpp>
 
@@ -18,6 +19,8 @@ private:
 	Gtk::Label *m_title;
 	GtkGraph *m_graph;
 	Gtk::Notebook *m_notebook;
+	Gtk::Box *m_stat_box;
+	Gtk::Box *m_piece_box;
 public:
 	GtkTorrentInfoBar();
 	void updateInfo(shared_ptr<gt::Torrent> selected);
