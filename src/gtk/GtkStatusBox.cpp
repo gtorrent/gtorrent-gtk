941d85c42f5b057355b0033b777b963ab7f56c03
IGI-111 <igi.222@gmail.com>
2014-08-14 19:35:32 +0200

    add new sections

--- src/gtk/GtkStatusBox.cpp
+++ src/gtk/GtkStatusBox.cpp
@@ -5,82 +5,154 @@
 GtkStatusBox::GtkStatusBox()
     : Gtk::Box(Gtk::ORIENTATION_VERTICAL, 5)
 {
-	m_transfer = Gtk::manage(new Gtk::Table(8, 4, false));
+
+    // Transfer
+	m_transfer = Gtk::manage(new Gtk::Table(8, 3, false));
 	m_transfer->set_col_spacings(5);
-
-    Gtk::Label *m_transfer_label = Gtk::manage(new Gtk::Label());
-	m_transfer_label->set_markup("<b><big>Transfer</big></b>");
-    this->add(*m_transfer_label);
-    this->add(*m_transfer);
-
     // Line 1
     Gtk::Label *m_time_elapsed_label = Gtk::manage(new Gtk::Label());
 	m_time_elapsed_label->set_markup("<b>Time Elapsed:</b>");
 	m_time_elapsed = Gtk::manage(new Gtk::Label());
-	m_transfer->attach(*m_time_elapsed_label, 0, 1, 1, 2, Gtk::AttachOptions::SHRINK);
-	m_transfer->attach(*m_time_elapsed, 1, 2, 1, 2, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_time_elapsed_label, 0, 1, 0, 1, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_time_elapsed, 1, 2, 0, 1, Gtk::AttachOptions::SHRINK);
 	
-    m_transfer->attach(*(new Gtk::VSeparator()), 2, 3, 1, 2, Gtk::AttachOptions::EXPAND);
+    m_transfer->attach(*(new Gtk::VSeparator()), 2, 3, 0, 1, Gtk::AttachOptions::EXPAND);
 
     Gtk::Label *m_remaining_label = Gtk::manage(new Gtk::Label());
 	m_remaining_label->set_markup("<b>Remaining:</b>");
 	m_remaining = Gtk::manage(new Gtk::Label());
-	m_transfer->attach(*m_remaining_label, 3, 4, 1, 2, Gtk::AttachOptions::SHRINK);
-	m_transfer->attach(*m_remaining, 4, 5, 1, 2, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_remaining_label, 3, 4, 0, 1, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_remaining, 4, 5, 0, 1, Gtk::AttachOptions::SHRINK);
 
-    m_transfer->attach(*(new Gtk::VSeparator()), 5, 6, 1, 2, Gtk::AttachOptions::EXPAND);
+    m_transfer->attach(*(new Gtk::VSeparator()), 5, 6, 0, 1, Gtk::AttachOptions::EXPAND);
 
     Gtk::Label *m_share_ratio_label = Gtk::manage(new Gtk::Label());
 	m_share_ratio_label->set_markup("<b>Ratio:</b>");
 	m_share_ratio = Gtk::manage(new Gtk::Label());
-	m_transfer->attach(*m_share_ratio_label, 6, 7, 1, 2, Gtk::AttachOptions::SHRINK);
-	m_transfer->attach(*m_share_ratio, 7, 8, 1, 2, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_share_ratio_label, 6, 7, 0, 1, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_share_ratio, 7, 8, 0, 1, Gtk::AttachOptions::SHRINK);
 
     // Line 2
     Gtk::Label *m_downloaded_label = Gtk::manage(new Gtk::Label());
 	m_downloaded_label->set_markup("<b>Downloaded:</b>");
 	m_downloaded = Gtk::manage(new Gtk::Label());
-	m_transfer->attach(*m_downloaded_label, 0, 1, 2, 3, Gtk::AttachOptions::SHRINK);
-	m_transfer->attach(*m_downloaded, 1, 2, 2, 3, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_downloaded_label, 0, 1, 1, 2, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_downloaded, 1, 2, 1, 2, Gtk::AttachOptions::SHRINK);
 	
-    m_transfer->attach(*(new Gtk::VSeparator()), 2, 3, 2, 3, Gtk::AttachOptions::EXPAND);
+    m_transfer->attach(*(new Gtk::VSeparator()), 2, 3, 1, 2, Gtk::AttachOptions::EXPAND);
 
     Gtk::Label *m_download_speed_label = Gtk::manage(new Gtk::Label());
 	m_download_speed_label->set_markup("<b>Download Speed:</b>");
 	m_download_speed = Gtk::manage(new Gtk::Label());
-	m_transfer->attach(*m_download_speed_label, 3, 4, 2, 3, Gtk::AttachOptions::SHRINK);
-	m_transfer->attach(*m_download_speed, 4, 5, 2, 3, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_download_speed_label, 3, 4, 1, 2, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_download_speed, 4, 5, 1, 2, Gtk::AttachOptions::SHRINK);
 
-    m_transfer->attach(*(new Gtk::VSeparator()), 5, 6, 2, 3, Gtk::AttachOptions::EXPAND);
+    m_transfer->attach(*(new Gtk::VSeparator()), 5, 6, 1, 2, Gtk::AttachOptions::EXPAND);
 
     Gtk::Label *m_down_limit_label = Gtk::manage(new Gtk::Label());
 	m_down_limit_label->set_markup("<b>Down Limit:</b>");
 	m_down_limit = Gtk::manage(new Gtk::Label());
-	m_transfer->attach(*m_down_limit_label, 6, 7, 2, 3, Gtk::AttachOptions::SHRINK);
-	m_transfer->attach(*m_down_limit, 7, 8, 2, 3, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_down_limit_label, 6, 7, 1, 2, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_down_limit, 7, 8, 1, 2, Gtk::AttachOptions::SHRINK);
 
     // Line 3
     Gtk::Label *m_uploaded_label = Gtk::manage(new Gtk::Label());
 	m_uploaded_label->set_markup("<b>Uploaded:</b>");
 	m_uploaded = Gtk::manage(new Gtk::Label());
-	m_transfer->attach(*m_uploaded_label, 0, 1, 3, 4, Gtk::AttachOptions::SHRINK);
-	m_transfer->attach(*m_uploaded, 1, 2, 3, 4, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_uploaded_label, 0, 1, 2, 3, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_uploaded, 1, 2, 2, 3, Gtk::AttachOptions::SHRINK);
 	
-    m_transfer->attach(*(new Gtk::VSeparator()), 2, 3, 3, 4, Gtk::AttachOptions::EXPAND);
+    m_transfer->attach(*(new Gtk::VSeparator()), 2, 3, 2, 3, Gtk::AttachOptions::EXPAND);
 
     Gtk::Label *m_upload_speed_label = Gtk::manage(new Gtk::Label());
 	m_upload_speed_label->set_markup("<b>Upload Speed:</b>");
 	m_upload_speed = Gtk::manage(new Gtk::Label());
-	m_transfer->attach(*m_upload_speed_label, 3, 4, 3, 4, Gtk::AttachOptions::SHRINK);
-	m_transfer->attach(*m_upload_speed, 4, 5, 3, 4, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_upload_speed_label, 3, 4, 2, 3, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_upload_speed, 4, 5, 2, 3, Gtk::AttachOptions::SHRINK);
 
-    m_transfer->attach(*(new Gtk::VSeparator()), 5, 6, 3, 4, Gtk::AttachOptions::EXPAND);
+    m_transfer->attach(*(new Gtk::VSeparator()), 5, 6, 2, 3, Gtk::AttachOptions::EXPAND);
 
     Gtk::Label *m_up_limit_label = Gtk::manage(new Gtk::Label());
 	m_up_limit_label->set_markup("<b>Up Limit:</b>");
 	m_up_limit = Gtk::manage(new Gtk::Label());
-	m_transfer->attach(*m_up_limit_label, 6, 7, 3, 4, Gtk::AttachOptions::SHRINK);
-	m_transfer->attach(*m_up_limit, 7, 8, 3, 4, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_up_limit_label, 6, 7, 2, 3, Gtk::AttachOptions::SHRINK);
+	m_transfer->attach(*m_up_limit, 7, 8, 2, 3, Gtk::AttachOptions::SHRINK);
+
+    // Tracker
+	m_tracker = Gtk::manage(new Gtk::Table(5, 1, false));
+	m_tracker->set_col_spacings(5);
+
+    // Line 1
+    Gtk::Label *m_tracker_url_label = Gtk::manage(new Gtk::Label());
+	m_tracker_url_label->set_markup("<b>Tracker URL:</b>");
+	m_tracker_url = Gtk::manage(new Gtk::Label());
+	m_tracker->attach(*m_tracker_url_label, 0, 1, 0, 1, Gtk::AttachOptions::SHRINK);
+	m_tracker->attach(*m_tracker_url, 1, 2, 0, 1, Gtk::AttachOptions::SHRINK);
+
+    m_tracker->attach(*(new Gtk::VSeparator()), 2, 3, 0, 1, Gtk::AttachOptions::EXPAND);
+
+    Gtk::Label *m_tracker_status_label = Gtk::manage(new Gtk::Label());
+	m_tracker_status_label->set_markup("<b>Tracker Status:</b>");
+	m_tracker_status = Gtk::manage(new Gtk::Label());
+	m_tracker->attach(*m_tracker_status_label, 3, 4, 0, 1, Gtk::AttachOptions::SHRINK);
+	m_tracker->attach(*m_tracker_status, 4, 5, 0, 1, Gtk::AttachOptions::SHRINK);
+
+    // General
+	m_general = Gtk::manage(new Gtk::Table(5, 3, false));
+	m_general->set_col_spacings(5);
+
+    // Line 1
+    Gtk::Label *m_save_as_label = Gtk::manage(new Gtk::Label());
+	m_save_as_label->set_markup("<b>Save As:</b>");
+	m_save_as = Gtk::manage(new Gtk::Label());
+	m_general->attach(*m_save_as_label, 0, 1, 0, 1, Gtk::AttachOptions::SHRINK);
+	m_general->attach(*m_save_as, 1, 2, 0, 1, Gtk::AttachOptions::SHRINK);
+
+    // Line 2
+    Gtk::Label *m_total_size_label = Gtk::manage(new Gtk::Label());
+	m_total_size_label->set_markup("<b>Total Size:</b>");
+	m_total_size = Gtk::manage(new Gtk::Label());
+	m_general->attach(*m_total_size_label, 0, 1, 1, 2, Gtk::AttachOptions::SHRINK);
+	m_general->attach(*m_total_size, 1, 2, 1, 2, Gtk::AttachOptions::SHRINK);
+
+    m_general->attach(*(new Gtk::VSeparator()), 2, 3, 1, 2, Gtk::AttachOptions::EXPAND);
+
+    Gtk::Label *m_pieces_label = Gtk::manage(new Gtk::Label());
+	m_pieces_label->set_markup("<b>Pieces:</b>");
+	m_pieces = Gtk::manage(new Gtk::Label());
+	m_general->attach(*m_pieces_label, 3, 4, 1, 2, Gtk::AttachOptions::SHRINK);
+	m_general->attach(*m_pieces, 4, 5, 1, 2, Gtk::AttachOptions::SHRINK);
+
+    // Line 3
+    Gtk::Label *m_lifetime_label = Gtk::manage(new Gtk::Label());
+	m_lifetime_label->set_markup("<b>Total Size:</b>");
+	m_lifetime = Gtk::manage(new Gtk::Label());
+	m_general->attach(*m_lifetime_label, 0, 1, 2, 3, Gtk::AttachOptions::SHRINK);
+	m_general->attach(*m_lifetime, 1, 2, 2, 3, Gtk::AttachOptions::SHRINK);
+
+    m_general->attach(*(new Gtk::VSeparator()), 2, 3, 2, 3, Gtk::AttachOptions::EXPAND);
+
+    Gtk::Label *m_hash_label = Gtk::manage(new Gtk::Label());
+	m_hash_label->set_markup("<b>Pieces:</b>");
+	m_hash = Gtk::manage(new Gtk::Label());
+	m_general->attach(*m_hash_label, 3, 4, 2, 3, Gtk::AttachOptions::SHRINK);
+	m_general->attach(*m_hash, 4, 5, 2, 3, Gtk::AttachOptions::SHRINK);
+
+
+    // Box setup
+    Gtk::Label *m_transfer_label = Gtk::manage(new Gtk::Label());
+	m_transfer_label->set_markup("<b><big>Transfer</big></b>");
+    Gtk::Label *m_tracker_label = Gtk::manage(new Gtk::Label());
+	m_tracker_label->set_markup("<b><big>Tracker</big></b>");
+    Gtk::Label *m_general_label = Gtk::manage(new Gtk::Label());
+	m_general_label->set_markup("<b><big>General</big></b>");
+    this->add(*m_transfer_label);
+    this->add(*m_transfer);
+    this->add(*m_tracker_label);
+    this->add(*m_tracker);
+    this->add(*m_general_label);
+    this->add(*m_general);
+
 }
 
 void GtkStatusBox::update(shared_ptr<gt::Torrent> selected)
@@ -91,7 +163,20 @@ void GtkStatusBox::update(shared_ptr<gt::Torrent> selected)
 
     m_downloaded->set_text(std::to_string(selected->getTotalDownloaded()));
     m_download_speed->set_text(std::to_string(selected->getDownloadRate()));
+    m_down_limit->set_text(std::to_string(0));
 
     m_uploaded->set_text(std::to_string(selected->getTotalUploaded()));
     m_upload_speed->set_text(std::to_string(selected->getUploadRate()));
+    m_up_limit->set_text(std::to_string(0));
+
+    m_tracker_url->set_text(std::to_string(0));
+    m_tracker_status->set_text(std::to_string(0));
+
+    m_save_as->set_text(std::to_string(0));
+
+    m_total_size->set_text(std::to_string(0));
+    m_pieces->set_text(std::to_string(0));
+
+    m_lifetime->set_text(std::to_string(0));
+    m_hash->set_text(std::to_string(0));
 }
--- src/gtk/GtkStatusBox.hpp
+++ src/gtk/GtkStatusBox.hpp
@@ -22,6 +22,21 @@ class GtkStatusBox : public Gtk::Box
     Gtk::Label *m_upload_speed;
     Gtk::Label *m_up_limit;
 
+    Gtk::Table *m_tracker;
+    // Line 1
+    Gtk::Label *m_tracker_url;
+    Gtk::Label *m_tracker_status;
+
+    Gtk::Table *m_general;
+    // Line 1
+    Gtk::Label *m_save_as;
+    // Line 2
+    Gtk::Label *m_total_size;
+    Gtk::Label *m_pieces;
+    // Line 3
+    Gtk::Label *m_lifetime;
+    Gtk::Label *m_hash;
+
     public:
     GtkStatusBox();
     void update(shared_ptr<gt::Torrent> selected);
