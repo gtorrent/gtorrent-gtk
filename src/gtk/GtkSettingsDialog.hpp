5dbdc95fc6fbd99427aef08d53390b81f0a8bccf
fuyukaidesu <hue@pomf.fr>
2014-08-18 04:23:37 +0200

    Add queuing support.
    Also modified a bit the settings menu, and added a setting I forgot before.

--- src/gtk/GtkSettingsDialog.hpp
+++ src/gtk/GtkSettingsDialog.hpp
@@ -24,37 +24,39 @@ class GtkSettingsDialog
 	GtkMainWindow *parent = nullptr;
 	std::map<std::string, std::string> backup;
 
-	Gtk::SpinButton        *uplimit,        *downlimit,  *dhtlimit, *cachesize,     *cacheexpiry, *cachechunk, *defaultreci, *increci, *decreci;//
-	Gtk::Entry             *useragentbox,   *hostbox,    *portbox,  *userbox,       *passbox,     *savepathbox;//
-	Gtk::ColorButton       *forebutt,       *backbutt,   *gridbutt, *downcolorbutt, *upcolorbutt;
-	Gtk::ComboBoxText      *seedchokecombo, *chokecombo, *statuscombo;
-	Gtk::Switch            *showtoggle,     *anontoggle, *suggesttoggle; //
-	Gtk::Button            *okbutt,         *notokbutt;//
-	Gtk::FileChooserButton *filebutt;//
+	Gtk::SpinButton        *uplimit,        *downlimit,  *dhtlimit,    *cachesize,     *cacheexpiry, *cachechunk, *defaultreci, *increci, *decreci, *activdspin, *activsspin;
+	Gtk::Entry             *useragentbox,   *hostbox,    *portbox,     *userbox,       *passbox,     *savepathbox;
+	Gtk::ColorButton       *forebutt,       *backbutt,   *gridbutt,    *downcolorbutt, *upcolorbutt;
+	Gtk::ComboBoxText      *seedchokecombo, *chokecombo, *statuscombo, *overridecombo;
+	Gtk::Switch            *showtoggle,     *anontoggle, *suggesttoggle;
+	Gtk::Button            *okbutt,         *notokbutt;
+	Gtk::FileChooserButton *filebutt;
 
 public:
 	GtkSettingsDialog(GtkMainWindow *Parent = nullptr);
-	void onOkClicked();
 	int run();
 
-	void onnotOkClicked();
-	void onUpLimitChanged();
-	void onDownLimitChanged();
-	void onDHTLimitChanged();
-	void onCacheSizeChanged();
+	void onOkClicked         ();
+	void onUpColorSet        ();
+	void onFilebuttSet       ();
+	void onGridColorSet      ();
+	void onDownColorSet      ();
+	void onChokeChanged      ();
+	void onnotOkClicked      ();
+	void onStatusChanged     ();
+	void onActivSChanged     ();
+	void onActivDChanged     ();
+	void onForegroundSet     ();
+	void onBackgroundSet     ();
+	void onIncreciChanged    ();
+	void onUpLimitChanged    ();
+	void onDecreciChanged    ();
+	void onOverrideChanged   ();
+	void onDHTLimitChanged   ();
+	void onCacheSizeChanged  ();
+	void onSeedChokeChanged  ();
+	void onDownLimitChanged  ();
+	void onCacheChunkChanged ();
 	void onCacheExpiryChanged();
-	void onCacheChunkChanged();
 	void onDefaultReciChanged();
-	void onIncreciChanged();
-	void onDecreciChanged();
-	void onFilebuttSet();
-	void onStatusChanged();
-
-	void onForegroundSet();
-	void onBackgroundSet();
-	void onGridColorSet();
-	void onDownColorSet();
-	void onUpColorSet();
-	void onChokeChanged();
-	void onSeedChokeChanged();
 };
