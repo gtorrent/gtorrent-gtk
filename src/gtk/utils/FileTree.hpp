9a189ba2c81fe911099ca720300477a41488bed5
nyanpasu <atago@pomf.se>
2014-08-22 15:14:25 +0800

    Whitespace + Reorder

--- src/gtk/utils/FileTree.hpp
+++ src/gtk/utils/FileTree.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 
 #include <libtorrent/file_storage.hpp>
+
 #include <gtorrent/Torrent.hpp>
 
 // It's safe to assume alphabetical order, so we can mix the
@@ -14,15 +15,17 @@
 class FileTree
 {
 public:
-	FileTree *parent = nullptr;
-	libtorrent::file_storage fs;
-	std::shared_ptr<gt::Torrent> t;
-	int index;
-	std::string filename;
-	std::string fullname();
-	std::map<std::string, FileTree*> children;
-	void add(std::string &str);
 	FileTree(libtorrent::file_storage files, std::shared_ptr<gt::Torrent> tor);
-	~FileTree();
 	FileTree(FileTree *Parent);
+	~FileTree();
+
+	std::string fullname();
+	void add(std::string &str);
+
+	int                              index;
+	FileTree                        *parent = nullptr;
+	libtorrent::file_storage         fs;
+	std::map<std::string, FileTree*> children;
+	std::shared_ptr<gt::Torrent>     t;
+	std::string                      filename;
 };
