ff23541c4bfe713f6d56db7d12bbfbb8dcd0e067
IGI-111 <igi.222@gmail.com>
2014-08-24 11:53:03 +0200

    fix for scrolling reset

--- src/gtk/GtkPeerTreeView.cpp
+++ src/gtk/GtkPeerTreeView.cpp
@@ -2,6 +2,28 @@
 
 #include <gtkmm/liststore.h>
 
+void GtkPeerTreeView::insert(const gt::Peer &p)
+{
+	Gtk::TreeModel::iterator i = m_liststore->children().begin();
+	while(i != m_liststore->children().end() && std::string((*i)[m_cols.m_col_ip]) < p.ip)
+		++i;
+	if(i != m_liststore->children().end() && std::string((*i)[m_cols.m_col_ip]) == p.ip)
+	{
+		(*i)[m_cols.m_col_ip] = p.ip;
+		(*i)[m_cols.m_col_client] = p.client;
+		(*i)[m_cols.m_col_down] = p.downloadSpeed;
+		(*i)[m_cols.m_col_up] = p.uploadSpeed;
+	}
+	else
+	{
+		i = m_liststore->insert(i);
+		(*i)[m_cols.m_col_ip] = p.ip;
+		(*i)[m_cols.m_col_client] = p.client;
+		(*i)[m_cols.m_col_down] = p.downloadSpeed;
+		(*i)[m_cols.m_col_up] = p.uploadSpeed;
+	}
+}
+
 GtkPeerTreeView::GtkPeerTreeView()
 {
 	m_liststore = Gtk::ListStore::create(m_cols);
@@ -12,22 +34,8 @@ GtkPeerTreeView::GtkPeerTreeView()
 
 void GtkPeerTreeView::update()
 {
-	m_liststore->clear();
-
 	for (auto &peer : torrent->getPeers())
-	{
-		Gtk::TreeModel::iterator i = m_liststore->append();
-		Gtk::TreeModel::Row row = *i;
-		std::cout << peer.ip << std::endl
-			<< peer.client << std::endl
-			<< peer.downloadSpeed << std::endl;
-		row[m_cols.m_col_ip] = peer.ip;
-		row[m_cols.m_col_client] = peer.client;
-		row[m_cols.m_col_down] = peer.downloadSpeed;
-		row[m_cols.m_col_up] = peer.uploadSpeed;
-
-	}
-
+		insert(peer);
 }
 
 void GtkPeerTreeView::setupColumns()
