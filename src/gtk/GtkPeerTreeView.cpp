67ce009ea46bf5f1029977826bd54096ae3c83ae
infoburp <gwolfendale@gmail.com>
2014-08-27 03:26:20 +0100

    tabs on peers box

--- src/gtk/GtkPeerTreeView.cpp
+++ src/gtk/GtkPeerTreeView.cpp
@@ -56,10 +56,25 @@ void GtkPeerTreeView::update()
 
 void GtkPeerTreeView::setupColumns()
 {
-	append_column("IP", m_cols.m_col_ip);
-	append_column("Client", m_cols.m_col_client);
-	append_column("Download", m_cols.m_col_down);
-	append_column("Upload", m_cols.m_col_up);
+	 append_column("IP", m_cols.m_col_ip);
+	 append_column("Port", m_cols.m_col_port);
+	 append_column("Client", m_cols.m_col_client);
+	 append_column("Flags", m_cols.m_col_flags);
+	 append_column("%", m_cols.m_col_percent);
+	 append_column("Relevance", m_cols.m_col_relevance);
+	 append_column("Down Speed", m_cols.m_col_down);
+	 append_column("Up Speed", m_cols.m_col_up);
+	 append_column("Reqs", m_cols.m_col_reqs);
+	 append_column("Waited", m_cols.m_col_waited);
+	 append_column("Downloaded", m_cols.m_col_downloaded);
+	 append_column("Uploaded", m_cols.m_col_uploaded);
+	 append_column("Hasherr", m_cols.m_col_hasherr);
+	 append_column("Peer Download Rate", m_cols.m_col_peer_download_rate);
+	 append_column("MaxDown", m_cols.m_col_max_down);
+	 append_column("MaxUp", m_cols.m_col_max_up);
+	 append_column("Queued", m_cols.m_col_queued);
+	 append_column("Inactive", m_cols.m_col_inactive);
+	 append_column("Debug", m_cols.m_col_debug);
 }
 
 void GtkPeerTreeView::select(std::shared_ptr<gt::Torrent> t)
--- src/gtk/GtkPeerTreeView.hpp
+++ src/gtk/GtkPeerTreeView.hpp
@@ -11,15 +11,46 @@ class GtkPeerColumns : public Gtk::TreeModel::ColumnRecord
 		GtkPeerColumns()
 		{
 			add(m_col_ip);
+			add(m_col_port);
 			add(m_col_client);
+			add(m_col_flags);
+			add(m_col_percent);
+			add(m_col_relevance);
 			add(m_col_down);
 			add(m_col_up);
+			add(m_col_reqs);
+			add(m_col_waited);
+			add(m_col_downloaded);
+			add(m_col_uploaded);
+			add(m_col_hasherr);
+			add(m_col_peer_download_rate);
+			add(m_col_max_down);
+			add(m_col_max_up);
+			add(m_col_queued);
+			add(m_col_inactive);
+			add(m_col_debug);
 		}
 
 		Gtk::TreeModelColumn<std::string> m_col_ip;
+		Gtk::TreeModelColumn<std::string> m_col_port;
 		Gtk::TreeModelColumn<std::string> m_col_client;
+		Gtk::TreeModelColumn<std::string> m_col_flags;
+		Gtk::TreeModelColumn<std::string> m_col_percent;
+		Gtk::TreeModelColumn<std::string> m_col_relevance;
 		Gtk::TreeModelColumn<std::string> m_col_down;
 		Gtk::TreeModelColumn<std::string> m_col_up;
+		Gtk::TreeModelColumn<std::string> m_col_reqs;
+		Gtk::TreeModelColumn<std::string> m_col_waited;
+		Gtk::TreeModelColumn<std::string> m_col_downloaded;
+		Gtk::TreeModelColumn<std::string> m_col_uploaded;
+		Gtk::TreeModelColumn<std::string> m_col_hasherr;
+		Gtk::TreeModelColumn<std::string> m_col_peer_download_rate;
+		Gtk::TreeModelColumn<std::string> m_col_max_down;
+		Gtk::TreeModelColumn<std::string> m_col_max_up;
+		Gtk::TreeModelColumn<std::string> m_col_queued;
+		Gtk::TreeModelColumn<std::string> m_col_inactive;
+		Gtk::TreeModelColumn<std::string> m_col_debug;
+
 };
 
 class GtkPeerTreeView : public Gtk::TreeView
