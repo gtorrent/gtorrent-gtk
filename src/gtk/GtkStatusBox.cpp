11da9d309649dc536634cd170781ea8c255314ae
fuyukaidesu <hue@pomf.fr>
2014-08-29 16:16:00 +0200

    Clean up Status box source and headers

--- src/gtk/GtkStatusBox.cpp
+++ src/gtk/GtkStatusBox.cpp
@@ -1,17 +1,9 @@
 #include <gtkmm/hvseparator.h>
-
 #include <gtorrent/Torrent.hpp>
-
 #include "GtkStatusBox.hpp"
-
-// TODO: remove all this boilerplate and replace it with initialize methods instead of doing the same thing a dozen times
 
 GtkStatusBox::GtkStatusBox(GtkGrid *grid, const Glib::RefPtr<Gtk::Builder> rbuilder) : Gtk::Grid(grid), builder(rbuilder)
 {
-//	set_halign         (Gtk::Align::ALIGN_CENTER);
-//	set_valign         (Gtk::Align::ALIGN_CENTER);
-//	set_column_spacing (10);
-
 	rbuilder->get_widget("DownSpeed"    , m_download_speed);
 	rbuilder->get_widget("Hash"         , m_hash          );
 	rbuilder->get_widget("Pieces"       , m_pieces        );
@@ -27,28 +19,6 @@ GtkStatusBox::GtkStatusBox(GtkGrid *grid, const Glib::RefPtr<Gtk::Builder> rbuil
 	rbuilder->get_widget("DownLimit"    , m_down_limit    );
 	rbuilder->get_widget("UpLimit"      , m_up_limit      );
 	rbuilder->get_widget("TrackerStatus", m_tracker_status);
-
-/*	attach(m_transfer            , 0, 0, 6, 1);
-	attach(m_time_elapsed        , 1, 1, 1, 1);
-	attach(m_share_ratio         , 3, 1, 1, 1);
-	attach(m_remaining           , 5, 1, 1, 1);
-	attach(m_downloaded          , 1, 2, 1, 1);
-	attach(m_download_speed      , 3, 2, 1, 1);
-	attach(m_down_limit          , 5, 2, 1, 1);
-	attach(m_uploaded            , 1, 3, 1, 1);
-	attach(m_upload_speed        , 3, 3, 1, 1);
-	attach(m_up_limit            , 5, 3, 1, 1);
-
-	attach(m_tracker             , 0, 4, 6, 1);
-	attach(m_tracker_url         , 1, 5, 1, 1);
-	attach(m_tracker_status      , 3, 5, 1, 1);
-
-	attach(m_general             , 0, 6, 6, 1);
-	attach(m_save_as             , 1, 7, 1, 1);
-	attach(m_total_size          , 1, 8, 1, 1);
-	attach(m_pieces              , 3, 8, 1, 1);
-	attach(m_hash                , 1, 9, 1, 1);
-*/
 }
 
 void GtkStatusBox::update(std::shared_ptr<gt::Torrent> selected)
--- src/gtk/GtkStatusBox.hpp
+++ src/gtk/GtkStatusBox.hpp
@@ -13,38 +13,19 @@ public:
 	void update(std::shared_ptr<gt::Torrent> selected);
 private:
 
-	Gtk::Label *m_transfer;
-	Gtk::Label *m_time_elapsed_label;
 	Gtk::Label *m_time_elapsed;
-	Gtk::Label *m_share_ratio_label;
 	Gtk::Label *m_share_ratio;
-	Gtk::Label *m_remaining_label;
 	Gtk::Label *m_remaining;
-	Gtk::Label *m_downloaded_label;
 	Gtk::Label *m_downloaded;
-	Gtk::Label *m_download_speed_label;
 	Gtk::Label *m_download_speed;
-	Gtk::Label *m_down_limit_label;
 	Gtk::Label *m_down_limit;
-	Gtk::Label *m_uploaded_label;
 	Gtk::Label *m_uploaded;
-	Gtk::Label *m_upload_speed_label;
 	Gtk::Label *m_upload_speed;
-	Gtk::Label *m_up_limit_label;
 	Gtk::Label *m_up_limit;
-	Gtk::Label *m_tracker;
-	Gtk::Label *m_tracker_url_label;
 	Gtk::Label *m_tracker_url;
-	Gtk::Label *m_tracker_status_label;
 	Gtk::Label *m_tracker_status;
-	Gtk::Label *m_general;
-	Gtk::Label *m_save_as_label;
 	Gtk::Label *m_save_as;
-	Gtk::Label *m_total_size_label;
 	Gtk::Label *m_total_size;
-	Gtk::Label *m_pieces_label;
 	Gtk::Label *m_pieces;
-	Gtk::Label *m_hash_label;
 	Gtk::Label *m_hash;
-
 };
