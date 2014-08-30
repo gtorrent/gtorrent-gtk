c286fa395cb728031f25b5106b55d2f6a6888bf5
infoburp <gwolfendale@gmail.com>
2014-08-30 23:24:42 +0100

    more clobbering

--- src/gtk/GtkMainWindow.cpp
+++ src/gtk/GtkMainWindow.cpp
@@ -26,96 +26,6 @@
 */
 GtkMainWindow::GtkMainWindow(GtkWindow *win, const Glib::RefPtr<Gtk::Builder> rbuilder) : Gtk::Window(win), builder(rbuilder), m_core(Application::getSingleton()->getCore())
 {
-	//TODO:This needs to be refactored
-	notify_init ("Hello world!");
-	set_position(Gtk::WIN_POS_CENTER);
-	set_default_size(800, 500);
-	magtxt->set_visible();
-	magtxt->set_width_chars(75);
-	magPop->add(*magtxt);
-	btn_add_link->set_popover(*magPop);
-
-	Gtk::Box *vbox = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_VERTICAL, 0));
-	add(*vbox);
-	Gtk::Paned *panel = Gtk::manage(new Gtk::Paned(Gtk::ORIENTATION_VERTICAL));
-
-	Gtk::MenuBar *menubar = Gtk::manage(new Gtk::MenuBar());
-	vbox->pack_start(*menubar, Gtk::PACK_SHRINK, 0);
-
-	Gtk::MenuItem *menuitem_file = Gtk::manage(new Gtk::MenuItem("_File", true));
-	menubar->append(*menuitem_file);
-	Gtk::MenuItem *menuitem_options = Gtk::manage(new Gtk::MenuItem("_Options", true));
-	menubar->append(*menuitem_options);
-	Gtk::MenuItem *menuitem_help = Gtk::manage(new Gtk::MenuItem("_Help", true));
-	menubar->append(*menuitem_help);
-
-	Gtk::Menu *filemenu = Gtk::manage(new Gtk::Menu());
-	menuitem_file->set_submenu(*filemenu);
-	Gtk::MenuItem *menuitem_add_torrent = Gtk::manage(new Gtk::MenuItem("_Add Torrent...", true));
-	filemenu->append(*menuitem_add_torrent);
-	Gtk::MenuItem *menuitem_add_torrent_nds = Gtk::manage(new Gtk::MenuItem("_Add Torrent (no default save)...", true));
-	filemenu->append(*menuitem_add_torrent_nds);
-	Gtk::MenuItem *menuitem_add_torrent_from_url = Gtk::manage(new Gtk::MenuItem("_Add Torrent from URL...", true));
-	filemenu->append(*menuitem_add_torrent_from_url);
-	Gtk::MenuItem *menuitem_add_rss_feed = Gtk::manage(new Gtk::MenuItem("_Add RSS Feed...", true));
-	filemenu->append(*menuitem_add_rss_feed);
-	Gtk::MenuItem *menuitem_create_new_torrent = Gtk::manage(new Gtk::MenuItem("_Create New Torrent...", true));
-	filemenu->append(*menuitem_create_new_torrent);
-	Gtk::MenuItem *menuitem_quit = Gtk::manage(new Gtk::MenuItem("_Exit", true));
-	filemenu->append(*menuitem_quit);
-
-	Gtk::Menu *optionsmenu = Gtk::manage(new Gtk::Menu());
-	menuitem_options->set_submenu(*optionsmenu);
-	Gtk::MenuItem *menuitem_preferences = Gtk::manage(new Gtk::MenuItem("_Preferences", true));
-	optionsmenu->append(*menuitem_preferences);
-	Gtk::MenuItem *menuitem_rss_downloader = Gtk::manage(new Gtk::MenuItem("_RSS Downloader", true));
-	optionsmenu->append(*menuitem_rss_downloader);
-	Gtk::MenuItem *menuitem_setup_guide = Gtk::manage(new Gtk::MenuItem("_Setup Guide", true));
-	optionsmenu->append(*menuitem_setup_guide);
-	Gtk::MenuItem *menuitem_show_toolbar = Gtk::manage(new Gtk::MenuItem("_Show Toolbar", true));
-	optionsmenu->append(*menuitem_show_toolbar);
-	Gtk::MenuItem *menuitem_show_detailed_info = Gtk::manage(new Gtk::MenuItem("_Show Detailed Info", true));
-	optionsmenu->append(*menuitem_show_detailed_info);
-	Gtk::MenuItem *menuitem_show_status_bar = Gtk::manage(new Gtk::MenuItem("_Show Status Bar", true));
-	optionsmenu->append(*menuitem_show_status_bar);
-	Gtk::MenuItem *menuitem_show_category_list = Gtk::manage(new Gtk::MenuItem("_Show Category List", true));
-	optionsmenu->append(*menuitem_show_category_list);
-	Gtk::MenuItem *menuitem_icons_on_tabs = Gtk::manage(new Gtk::MenuItem("_Icons on Tabs", true));
-	optionsmenu->append(*menuitem_icons_on_tabs);
-	Gtk::MenuItem *menuitem_auto_shutdown = Gtk::manage(new Gtk::MenuItem("_Auto Shutdown", true));
-	optionsmenu->append(*menuitem_auto_shutdown);
-
-	Gtk::Menu *helpmenu = Gtk::manage(new Gtk::Menu());
-	menuitem_help->set_submenu(*helpmenu);
-	Gtk::MenuItem *menuitem_gtorrent_help = Gtk::manage(new Gtk::MenuItem("_gTorrent Help", true));
-	helpmenu->append(*menuitem_gtorrent_help);
-	Gtk::MenuItem *menuitem_gtorrent_faq = Gtk::manage(new Gtk::MenuItem("_gTorrent FAQ", true));
-	helpmenu->append(*menuitem_gtorrent_faq);
-	Gtk::MenuItem *menuitem_gtorrent_webpage = Gtk::manage(new Gtk::MenuItem("_gTorrent Webpage", true));
-	helpmenu->append(*menuitem_gtorrent_webpage);
-	Gtk::MenuItem *menuitem_gtorrent_forums = Gtk::manage(new Gtk::MenuItem("_gTorrent Forums", true));
-	helpmenu->append(*menuitem_gtorrent_forums);
-	Gtk::MenuItem *menuitem_check_for_updates = Gtk::manage(new Gtk::MenuItem("_Check for Updates", true));
-	helpmenu->append(*menuitem_check_for_updates);
-	Gtk::MenuItem *menuitem_download_translation = Gtk::manage(new Gtk::MenuItem("_Download Translation", true));
-	helpmenu->append(*menuitem_download_translation);
-	Gtk::MenuItem *menuitem_show_statistics = Gtk::manage(new Gtk::MenuItem("_Show Statistics", true));
-	helpmenu->append(*menuitem_show_statistics);
-	Gtk::MenuItem *menuitem_about_gtorrent = Gtk::manage(new Gtk::MenuItem("_About gTorrent", true));
-	helpmenu->append(*menuitem_about_gtorrent);
-
-	m_status_label = Gtk::manage(new Gtk::Label());
-	m_status_label->set_markup("<b>Status Bar</b>");
-	m_status_bar->add(*m_status_label);
-	vbox->pack_end(*m_status_bar, Gtk::PACK_SHRINK, 0);
-
-	magtxt->set_visible();
-	magtxt->set_width_chars(75);
-	magPop->add(*magtxt);
-	btn_add_link->set_popover(*magPop);
-
-	m_swin = Gtk::manage(new Gtk::ScrolledWindow());
-
 	notify_init ("gTorrent");
 
 	builder->get_widget( "addTorrentButton", addTorrentButton);
@@ -127,11 +37,8 @@ GtkMainWindow::GtkMainWindow(GtkWindow *win, const Glib::RefPtr<Gtk::Builder> rb
 	builder->get_widget(   "settingsButton",   settingsButton);
 	builder->get_widget(            "panel",            panel);
 	builder->get_widget(   "scrolledWindow",   scrolledWindow);
-	builder->get_widget(          "vSepZero",   	 separator0);
-	builder->get_widget(          "vSepOne",   		 separator1);
-	builder->get_widget(          "vSepTwo",   		 separator2);
-	builder->get_widget(          "vSepThree",   	 separator3);
-	builder->get_widget(          "vSepFour",   	 separator4);
+	builder->get_widget(          "vSepOne",    vSeparatorOne);
+	builder->get_widget(          "vSepTwo",    vSeparatorTwo);
 	builder->get_widget_derived(  "infobar",        m_infobar);
 
 	magEntry   = Gtk::manage(new Gtk::Entry());
@@ -150,74 +57,8 @@ GtkMainWindow::GtkMainWindow(GtkWindow *win, const Glib::RefPtr<Gtk::Builder> rb
 	panel->pack2(*m_infobar);
 
 	Glib::signal_timeout().connect_seconds(sigc::mem_fun(*this, &GtkMainWindow::onSecTick), 1);
-	signal_delete_event().connect(sigc::mem_fun(*this, &GtkMainWindow::onDestroy));
-	signal_key_press_event().connect(sigc::mem_fun(*this, &GtkMainWindow::onKeyPress), false);
+	this->signal_delete_event().connect(sigc::mem_fun(*this, &GtkMainWindow::onDestroy));
 
-	header = Gtk::manage(new Gtk::HeaderBar());
-	header->set_show_close_button(true);
-	header->set_title("gTorrent");
-
-	Gtk::VSeparator *separator0  = Gtk::manage(new Gtk::VSeparator());
-	Gtk::VSeparator *separator1  = Gtk::manage(new Gtk::VSeparator());
-	Gtk::VSeparator *separator2  = Gtk::manage(new Gtk::VSeparator());
-	Gtk::VSeparator *separator3  = Gtk::manage(new Gtk::VSeparator());
-	Gtk::VSeparator *separator4  = Gtk::manage(new Gtk::VSeparator());
-
-	btn_add_link   ->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onAddMagnetBtnClicked));
-	btn_add_torrent->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onAddBtnClicked));
-
-	btn_pause      ->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onPauseBtnClicked));
-	btn_resume     ->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onResumeBtnClicked));
-	btn_remove     ->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onRemoveBtnClicked));
-
-	btn_settings   ->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onSettingsBtnClicked));
-
-	btn_add_link   ->set_image_from_icon_name("insert-link-symbolic");
-	btn_add_torrent->set_image_from_icon_name("list-add-symbolic");
-	btn_add_rss->set_image_from_icon_name("folder-remote-symbolic");
-
-	btn_create_torrent->set_image_from_icon_name("text-x-generic");
-
-	btn_remove     ->set_image_from_icon_name("edit-delete-symbolic");
-
-	btn_resume     ->set_image_from_icon_name("media-playback-start-symbolic");
-	btn_pause      ->set_image_from_icon_name("media-playback-pause-symbolic");
-	btn_stop      	->set_image_from_icon_name("media-playback-stop-symbolic");
-
-	btn_up      	->set_image_from_icon_name("go-up");
-	btn_down      	->set_image_from_icon_name("go-down");
-
-	btn_settings   ->set_image_from_icon_name("emblem-system-symbolic");
-
-	//TODO:align properties button to right of top bar
-	//btn_properties->set_alignment(1.0f,0.0f);
-	header->add(*btn_add_torrent);
-	header->add(*btn_add_link);
-	header->add(*btn_add_rss);
-
-	header->add(*separator0);
-
-	header->add(*btn_create_torrent);
-
-	header->add(*separator1);
-
-	header->add(*btn_remove);
-
-	header->add(*separator2);
-
-	header->add(*btn_resume);
-	header->add(*btn_pause);
-	header->add(*btn_stop);
-
-	header->add(*separator3);
-
-	header->add(*btn_up);
-	header->add(*btn_down);
-
-	header->add(*separator4);
-
-	header->add(*btn_settings);
-	//header->pack_end(*btn_settings);
 	addTorrentButton->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onAddBtnClicked       ));
 	pauseButton     ->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onPauseBtnClicked     ));
 	resumeButton    ->signal_clicked().connect(sigc::mem_fun(*this, &GtkMainWindow::onResumeBtnClicked    ));
@@ -235,19 +76,6 @@ GtkMainWindow::GtkMainWindow(GtkWindow *win, const Glib::RefPtr<Gtk::Builder> rb
 
 	m_treeview->drag_dest_set(listTargets, Gtk::DEST_DEFAULT_MOTION | Gtk::DEST_DEFAULT_DROP, Gdk::ACTION_COPY | Gdk::ACTION_MOVE | Gdk::ACTION_LINK | Gdk::ACTION_PRIVATE);
 	m_treeview->signal_drag_data_received().connect(sigc::mem_fun(*this, &GtkMainWindow::onFileDropped));
-
-	vbox->add(*header);
-
-	//set_titlebar(*header);
-
-	vbox->add(*panel);
-
-	//add(*panel);
-	show_all();
-	//btn_pause->hide();
-	//m_infobar->set_visible(false);
-
-	// for some reason, the treeview start with its first element selected
 
 	m_treeview->get_selection()->unselect_all();
 
@@ -449,18 +277,4 @@ bool GtkMainWindow::onDestroy(GdkEventAny *event)
 void GtkMainWindow::onSettingsBtnClicked()
 {
 	d->run();
-}
-
-bool GtkMainWindow::onKeyPress(GdkEventKey *event)
-{
-	short arrowkeys[] = { 80, 88, 83, 85, 111, 114, 113, 116 };
-	short enter[] = { 36, 104 };
-	if(std::find(arrowkeys, arrowkeys + 8, event->hardware_keycode) == arrowkeys + 8)
-	{
-		m_treeview->m_searchPopover->set_visible();
-		if(std::find(enter, enter + 2, event->hardware_keycode) != enter + 2)
-			m_treeview->m_searchPopover->set_visible(false);
-		m_treeview->m_filter->refilter();
-	}
-	return false;
 }
--- src/gtk/GtkMainWindow.hpp
+++ src/gtk/GtkMainWindow.hpp
@@ -1,10 +1,6 @@
 #pragma once
 
 #include <gtorrent/Core.hpp>
-
-#include <gtkmm/table.h>
-#include <gtkmm/statusbar.h>
-#include <gtkmm/menubar.h>
 #include <gtkmm/builder.h>
 #include <gtkmm/button.h>
 #include <gtkmm/entry.h>
@@ -27,7 +23,6 @@ class GtkMainWindow : public Gtk::Window
 	GtkSettingsDialog *d = nullptr;
 
 	// Signal Responders
-	bool onKeyPress(GdkEventKey *event);
 	void onAboutBtnClicked();
 	void onAddBtnClicked();
 	void onAddMagnetBtnClicked();
@@ -40,12 +35,11 @@ class GtkMainWindow : public Gtk::Window
 	void onFileDropped(const Glib::RefPtr<Gdk::DragContext>& context, int x, int y, const Gtk::SelectionData& selection_data, guint info, guint time);
 
 public:
-	GtkTorrentTreeView 	*m_treeview = nullptr;
-	GtkTorrentInfoBar  	*m_infobar = nullptr;
-	Gtk::ScrolledWindow *m_swin;
-	Gtk::HeaderBar			*header;
+	GtkTorrentTreeView *m_treeview = nullptr;
+	GtkTorrentInfoBar  *m_infobar = nullptr;
+
 	Gtk::Button *addTorrentButton = nullptr, *resumeButton = nullptr, *pauseButton = nullptr, *removeButton = nullptr, *propertiesButton = nullptr, *settingsButton = nullptr;
-	Gtk::Separator *separator0 = nullptr, *separator1 = nullptr, *separator2 = nullptr, *separator3 = nullptr, *separator4 = nullptr;
+	Gtk::Separator *vSeparatorOne = nullptr, *vSeparatorTwo = nullptr;
 	Gtk::Popover *magPopover = nullptr;
 	Gtk::Entry *magEntry = nullptr;
 	Gtk::ScrolledWindow *scrolledWindow = nullptr;
@@ -55,26 +49,18 @@ public:
 
 //	Gtk::ScrolledWindow *m_swin;
 
-	Gtk::MenuButton *btn_add_link = Gtk::manage(new Gtk::MenuButton());
+/*	Gtk::MenuButton *btn_add_link = Gtk::manage(new Gtk::MenuButton());
 	Gtk::Entry      *magtxt       = Gtk::manage(new Gtk::Entry());
 	//We've Always Been Shameless About Stealing Great Ideas
 	Gtk::Popover    *magPop = Gtk::manage(new Gtk::Popover(*btn_add_link)); // We may need to provide a fallback CSS for those who use old themes
 	// thx prince
-	Gtk::Button 		*btn_add_rss  			= Gtk::manage(new Gtk::Button());
-	Gtk::Button 		*btn_create_torrent	= Gtk::manage(new Gtk::Button());
-	Gtk::Button 		*btn_stop        		= Gtk::manage(new Gtk::Button());
-	Gtk::Button 		*btn_up        			= Gtk::manage(new Gtk::Button());
-	Gtk::Button 		*btn_down        		= Gtk::manage(new Gtk::Button());
-	Gtk::Statusbar 	*m_status_bar				= Gtk::manage(new Gtk::Statusbar());
-	Gtk::Label 			*m_status_label;
-	Gtk::Table 			*m_main_table_layout;
 	Gtk::Button *btn_add_torrent  = Gtk::manage(new Gtk::Button());
 	Gtk::Button *btn_pause        = Gtk::manage(new Gtk::Button());
 	Gtk::Button *btn_properties   = Gtk::manage(new Gtk::Button());
 	Gtk::Button *btn_remove       = Gtk::manage(new Gtk::Button());
 	Gtk::Button *btn_resume       = Gtk::manage(new Gtk::Button());
 	Gtk::Button *btn_settings     = Gtk::manage(new Gtk::Button());
-
+*/
 
 	GtkMainWindow(GtkWindow*, const Glib::RefPtr<Gtk::Builder>);
 	bool onDestroy(GdkEventAny *event);
