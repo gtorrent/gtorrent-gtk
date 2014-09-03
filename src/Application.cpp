94eea985ce0b69bd205a0fd12546fa4f75c5c3d5
infoburp <gwolfendale@gmail.com>
2014-09-03 16:55:18 +0100

    fix other typo

--- src/Application.cpp
+++ src/Application.cpp
@@ -19,7 +19,7 @@ shared_ptr<gt::Core> &Application::getCore()
 
 int Application::run(int argc, char **argv)
 {
-	gt::Log::Debug("GTorrent v." GTORRENT_VERSION);
+	gt::Log::Debug("gTorrent v." GTORRENT_VERSION);
 	gt::Log::Debug("Starting up core library...");
 	m_gui->loadDefaultSettings();
 	m_core = make_shared<gt::Core>(argc, argv);
