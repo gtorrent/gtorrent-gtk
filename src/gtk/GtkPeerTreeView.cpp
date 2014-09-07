37a28005d20d85268f3cbf2f3cc3209939dfb38e
IGI-111 <igi.222@gmail.com>
2014-09-08 00:00:21 +0200

    fix ip sorting

--- src/gtk/GtkPeerTreeView.cpp
+++ src/gtk/GtkPeerTreeView.cpp
@@ -2,27 +2,25 @@
 
 #include <gtkmm/liststore.h>
 
-// Sort will be a bit fucked because the Peer class was written without being able to keep numerical values of the speed
-// nor the ip, so for example 2 B/S is considered faster than 100 kB/s and 123.42.21.54 is considered lower than 82.12.54.31
 void GtkPeerTreeView::insert(const gt::Peer &p)
 {
 	Gtk::TreeModel::iterator i = m_liststore->children().begin();
-	while(i != m_liststore->children().end() && std::string((*i)[m_cols.m_col_ip]) < p.getIP())
+	while(i != m_liststore->children().end() && boost::asio::ip::address::from_string(std::string((*i)[m_cols.m_col_ip])) < p.getIP())
 		++i;
-	if(i != m_liststore->children().end() && std::string((*i)[m_cols.m_col_ip]) == p.getIP())
+	if(i != m_liststore->children().end() && boost::asio::ip::address::from_string(std::string((*i)[m_cols.m_col_ip])) == p.getIP())
 	{
-		(*i)[m_cols.m_col_ip] = p.getIP();
+		(*i)[m_cols.m_col_ip] = p.getIP().to_string();
 		(*i)[m_cols.m_col_client] = p.getClient();
-		(*i)[m_cols.m_col_down] = p.getDownloadSpeed();
-		(*i)[m_cols.m_col_up] = p.getUploadSpeed();
+		(*i)[m_cols.m_col_down] = getRateString(p.getDownloadSpeed());
+		(*i)[m_cols.m_col_up] = getRateString(p.getUploadSpeed());
 	}
 	else
 	{
 		i = m_liststore->insert(i);
-		(*i)[m_cols.m_col_ip] = p.getIP();
+		(*i)[m_cols.m_col_ip] = p.getIP().to_string();
 		(*i)[m_cols.m_col_client] = p.getClient();
-		(*i)[m_cols.m_col_down] = p.getDownloadSpeed();
-		(*i)[m_cols.m_col_up] = p.getUploadSpeed();
+		(*i)[m_cols.m_col_down] = getRateString(p.getDownloadSpeed());
+		(*i)[m_cols.m_col_up] = getRateString(p.getUploadSpeed());
 	}
 }
 
@@ -45,16 +43,16 @@ void GtkPeerTreeView::setupColumns()
 	int cid;
 	Gtk::TreeViewColumn *col;
 	cid = append_column("IP"      , m_cols.m_col_ip);
-    col = get_column(cid - 1);
+	col = get_column(cid - 1);
 	col->set_sort_column(m_cols.m_col_ip);
 	cid = append_column("Client"  , m_cols.m_col_client);
-    col = get_column(cid - 1);
+	col = get_column(cid - 1);
 	col->set_sort_column(m_cols.m_col_client);
 	cid = append_column("Download", m_cols.m_col_down);
-    col = get_column(cid - 1);
+	col = get_column(cid - 1);
 	col->set_sort_column(m_cols.m_col_down);
 	cid = append_column("Upload"  , m_cols.m_col_up);
-    col = get_column(cid - 1);
+	col = get_column(cid - 1);
 	col->set_sort_column(m_cols.m_col_up);
 }
 
